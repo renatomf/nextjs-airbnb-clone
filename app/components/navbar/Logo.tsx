5d8f5cd60343b5135970c0f197d2196b22026ec6
Renato <renato.marques@brlink.com.br>
2024-02-06 11:29:37 -0300

    categories ui

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -7,13 +7,14 @@ const Logo = () => {
   const router = useRouter();
 
   return (
-    <Image 
-        alt="Logo"
-        className="hidden md:block cursor-pointer"
-        height="100"
-        width="100"
-        src="/images/logo.png"
-        priority={true}
+    <Image
+      onClick={() => router.push('/')}
+      alt="Logo"
+      className="hidden cursor-pointer md:block"
+      height="100"
+      width="100"
+      src="/images/logo.png"
+      priority={true}
     />
   )
 }
