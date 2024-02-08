99fc8ca8c2bdb1d40ddc575a494e89ffa5fa49fc
Renato <renato.marques@brlink.com.br>
2024-02-07 21:35:37 -0300

    reservation functionality

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -4,13 +4,13 @@ export default async function getListings() {
   try {
     const listings = await prisma.listing.findMany({
       orderBy: {
-        createAt: 'desc'
+        createdAt: 'desc'
       }
     });
 
     const safeListings = listings.map((listing) => ({
       ...listing,
-      createAt: listing.createAt.toISOString(),
+      createdAt: listing.createdAt.toISOString(),
     }));
 
     return safeListings;
